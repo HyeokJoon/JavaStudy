cf179875fdf686ddedac6152695612f6e38f2692
kimhyeokjoon <fufu98@naver.com>
2024-01-15 16:39:43 +0900

    더미 데이터 생성 방법

--- JavaTest/src/com/test/java/Ex50_Random.java
+++ JavaTest/src/com/test/java/Ex50_Random.java
@@ -8,7 +8,36 @@ public class Ex50_Random {
 		//1. Math.random()
 		//2. Random 클래스
 		
-		m1();
+//		m1();
+		m2();
+		
+	}
+
+	private static void m2() {
+		
+		Random rnd = new Random();
+		//문자열 난수 > 배열
+		String[] name1 = {"김","이","박","최","정","유","민","차","강","장","조","전"};
+		String[] name2 = {"길","동","재","석","명","수","진","원","호","미","주","이","경","준","혁","상","민"};
+		
+		String[] address1 = {"서울시","인천시","부산시","강릉시","대구시","광주시","제주시"};
+		String[] address2 = {"강남구", "강동구", "강서구", "강북구", "중구"};
+		String[] address3 = {"역삼동", "대치동", "압구정동", "천호동", "길동", "명일동"};
+		for(int i=0; i<200; i++) {
+			String name = name1[rnd.nextInt(name1.length-1)] 
+						+ name2[rnd.nextInt(name2.length-1)]
+						+ name2[rnd.nextInt(name2.length-1)];
+			
+			int age = rnd.nextInt(2) +13;
+			
+			String gender = rnd.nextInt(2) == 0 ? "남자" : "여자";
+			
+			String address = address1[rnd.nextInt(address1.length-1)] + " "
+							+ address2[rnd.nextInt(address2.length-1)] + " "
+							+address3[rnd.nextInt(address3.length-1)] + " ";
+			
+			System.out.printf("%s(%d세) %s %s\n",name,age,gender,address);
+		}
 		
 	}
 
